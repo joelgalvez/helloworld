96308eeaa785ce747e6b9691393d80edb18ade74
Joel Galvez <joelgalvez@gmail.com>
2015-03-26 07:34:12 +0100

    read more if content

--- site/templates/blocks.php
+++ site/templates/blocks.php
@@ -7,6 +7,11 @@
 			<?if($child->template->name=='article'):?>
 				<div class="page <?=$child->template->name?>">
 					<div class="leadtext"><?=$child->leadtext?></div>
+					<?if($child->getUnformatted('body')):?>
+						<div class="readmore">
+							<a href="<?=$child->url?>"><?=$child->readmore?$child->readmore:'Read more'?></a>
+						</div>
+					<?endif?>
 				</div>
 			<?else:?>
 				<?=$child->render()?>
