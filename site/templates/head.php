fd57fe0c2e002b8c7dfece650fe4252194bab1bd
Joel Galvez <joelgalvez@gmail.com>
2015-05-04 13:39:19 +0200

    no nav tag if no nav

--- site/templates/head.php
+++ site/templates/head.php
@@ -28,34 +28,34 @@
 				}
 				
 			?>
-			<div class="nav content">
-				<?
-					$parents_or_self = new PageArray();
-					$parents_or_self->add($page->parents());
-					$parents_or_self->add($page);
-
-				?>
-				<?foreach($parents_or_self as $pos):?>
-					<h2>
-						<?foreach($pos->siblings() as $sibling):?>
-							<?if($sibling->inmenu):?>
-								<a class="nav-item <?=$sibling->id==$pos->id?'active':''?>" href="<?=$sibling->url?>"><?=$sibling->title?></a>
-							<?endif?>
-						<?endforeach?>
-					</h2>
-				<?endforeach?>
-				<?
-				$subs = $page->children();
-				?>
-				<?if(count($subs)>0):?>
-					<h2>
-						<?foreach($subs as $c):?>
-							<?if($c->inmenu):?>
-								<a class="nav-item" href="<?=$c->url?>"><?=$c->title?></a>
-							<?endif?>
-						<?endforeach?>
-					</h2>
-				<?endif?>
-
-			</div>
+			<?
+				$parents_or_self = new PageArray();
+				$parents_or_self->add($page->parents());
+				$parents_or_self->add($page);
+			?>
+			<?if(count($parents_or_self)>0):?>
+				<div class="nav content">
+					<?foreach($parents_or_self as $pos):?>
+						<h2>
+							<?foreach($pos->siblings() as $sibling):?>
+								<?if($sibling->inmenu):?>
+									<a class="nav-item <?=$sibling->id==$pos->id?'active':''?>" href="<?=$sibling->url?>"><?=$sibling->title?></a>
+								<?endif?>
+							<?endforeach?>
+						</h2>
+					<?endforeach?>
+					<?
+					$subs = $page->children();
+					?>
+					<?if(count($subs)>0):?>
+						<h2>
+							<?foreach($subs as $c):?>
+								<?if($c->inmenu):?>
+									<a class="nav-item" href="<?=$c->url?>"><?=$c->title?></a>
+								<?endif?>
+							<?endforeach?>
+						</h2>
+					<?endif?>
+				</div>
+			<?endif?>
 <?endif?>
