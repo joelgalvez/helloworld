b100653c9d5eebdd54a5baab09bb53911556b7d7
Joel Galvez <joelgalvez@gmail.com>
2016-02-29 14:31:51 +0100

    - language switch - removed switches

--- site/templates/article.php
+++ site/templates/article.php
@@ -4,8 +4,20 @@
 	<?endif?>
 
 	<div class="page <?=$page->template->name?>">
-		<!-- <h1><?=$page->title?></h1> -->
-		<!-- <div class="overviewtext content"><?=$page->overviewtext?></div> -->
-		<div class="body content"><?=$page->body?></div>
+		<div class="content">
+			<h1><?=$page->title?></h1>
+			<?if(count($page->images)>0):?>
+				<?
+					$thumb = $page->images->first()->width(2880, array('upscaling'=>false, 'cropping'=>false));
+				?>
+				<div class="image">
+					<img src="<?=$thumb->url?>" alt="<?=$image->description?>">
+					<div class="caption"><?=$image->description?>
+				</div>
+			<?endif?>
+			<div class="overviewtext"><?=$page->overviewtext?></div>
+			<div class="body"><?=$page->body?></div>
+		</div>
+
 	</div>
 <?include('foot.php');?>
